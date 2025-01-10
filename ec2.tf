049d1f40e6a06311f3c18aede79a25bd012da29d
WARM0419 <warm624.h2@gmail.com>
2025-01-10 13:44:36 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web_server" {
   instance_type          = "t2.micro"              # Change instance type if needed
   key_name               = "project3"              # Change to your key pair name
   subnet_id              = aws_subnet.public_subnet_az1.id
-  vpc_securiyu_group_ids = [aws_security_group.project_sg.id]
+  vpc_security_group_ids = [aws_security_group.project_sg.id]
   user_data              = file("command.sh")
 
   tags = {
