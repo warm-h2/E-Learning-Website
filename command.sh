1397993414899afb5e76e1a9dc5da789061481e3
WARM0419 <warm624.h2@gmail.com>
2025-01-10 15:09:58 +0000

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,14 +1,25 @@
-# our script tht can helps us to deploy appache and our E-commerce store.
-#!/bin/bash 
-sudo su                                                          #root user
-yum update -y                                                    #update the package manager
-yum install -y httpd                                             #install appache server
-mkdir store-dir                                                  #create directory
-cd store-dir                                                     #change into the created file 
-wget https://www.free-css.com/assets/files/free-css-templates/download/page291/elearning.zip #download template
-unzip elearning.zip                                                 #unzip the file
-cd elearning-html-template                                        #change directory into the unzip file                                             
-mv * /var/www/html/   #move the file to appache directory                                            #
-cd /var/www/html/      #switch to appache path
-systemctl enable httpd  #to enable appache webserver
-systemctl start httpd   #to start appache webserver
+#!/bin/bash
+# Script to deploy Apache and set up an e-commerce store
+
+# Update the package manager
+yum update -y
+
+# Install Apache server and unzip utility
+yum install -y httpd unzip
+
+# Create a directory for the store if it doesn't exist
+mkdir -p /home/ec2-user/store-dir
+cd /home/ec2-user/store-dir
+
+# Download the template
+wget https://www.free-css.com/assets/files/free-css-templates/download/page291/elearning.zip
+
+# Unzip the file
+unzip elearning.zip
+
+# Move the files to the Apache directory
+mv elearning-html-template/* /var/www/html/
+
+# Enable and start the Apache webserver
+systemctl enable httpd
+systemctl start httpd
